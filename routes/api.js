8900cc67513636325ba604a390c26182932c16e0
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-14 17:54:04 -0600

    added helper folder_created uuidJS_fsUtils

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-
-
+const uuid = require('../helpers/uuid')
 
 router.get("/notes", (req, res) => {
     fs.readFile("./db/db.json", 'utf8',(data)=>{
@@ -12,8 +11,33 @@ router.get("/notes", (req, res) => {
     })
     
     
-router.post("/notes", (req, res) => {
-        const apiNote = JSON.parse(fs.readFile)
-    })
+// router.post("/notes", (req, res) => {
+//         const apiNote = JSON.parse(fs.readFile)
+//     })
+// router.post("/notes", (req, res) => {
 
-    module.exports=router
+// });
+
+router.post("/notes", (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid()
+    }
+    fsUtils.readAndAppend(newNote, './db/db.json')
+    const response = {
+        status: "success", body: newNote
+    }
+    res.json(response);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    fsUtils
+        .removeNote(req.params.id, './db/db.json')
+    const response = {
+        status: "success"
+    }
+    res.json(response);;
+  });
+
+module.exports=router
