e944e3466e066c1a21f24bb92306eeefc9e8b034
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-15 11:23:20 -0600

    notes now show up in dbJSON_no id number though and cant save note online

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,26 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const notes = require("../db/db");
+const { builtinModules } = require("module");
 
 router.get("/notes", (req, res) => {
     fs.readFile("./db/db.json", 'utf8',(data)=>{
         let apiNote = JSON.parse(data)
         res.json(apiNote)
     })
-    })
+    });
     
     
 // router.post("/notes", (req, res) => {
 //         const apiNote = JSON.parse(fs.readFile)
 //     })
 router.post("/notes", (req, res) => {
-    res.json({
-        "title": "Batman"
-    })
+    let request = req.body;
+    // req.body.id = router;
+    notes.push(request);
+    res.json(notes);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes))
 })
 
-module.exports=router
+module.exports=router;
